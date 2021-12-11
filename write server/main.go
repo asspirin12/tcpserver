be21d00ade585875e1b171592f6f7330918dc213
Sergey Kozlovskiy <aspirin12@yandex.ru>
2021-12-11 15:22:59 +0300

    a simple server that writes to a conn

---
+++ write server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "net"
+)
+
+func must(err error) {
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func main() {
+    listener, err := net.Listen("tcp", "localhost:8080")
+    must(err)
+
+    defer listener.Close()
+
+    for {
+        conn, err := listener.Accept()
+        must(err)
+
+        _, err = conn.Write([]byte("Hello world"))
+        must(err)
+
+        conn.Close()
+    }
+}
