34ea4462e43587733b8e3e812b1bb6a65edf3e93
Sergey Kozlovskiy <aspirin12@yandex.ru>
2021-12-12 16:49:11 +0300

    add read and write server

---
+++ read_write_server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func handle(conn net.Conn) {
+	for {
+		messageFromClient, err := bufio.NewReader(conn).ReadString('\n')
+		must(err)
+
+		fmt.Print(string(messageFromClient))
+
+		fmt.Fprintf(conn, "Server received a message: %s", messageFromClient)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:3333")
+	must(err)
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		must(err)
+
+		defer conn.Close()
+
+		handle(conn)
+	}
+
+}
